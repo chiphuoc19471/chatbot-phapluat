3e175f1d62c4983b5f0fec613d0536e94b7bea17
Bùi Thái Học <hocbui27@gmail.com>
2026-06-09 21:31:30 +0700

    Update frontend files 6/9

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,6 +1,6 @@
 // js/config.js
 const CONFIG = {
     BASE_URL: 'http://localhost:8000',
-    USE_MOCK: true,   // set false khi có backend thật
+    USE_MOCK: false,   // set false khi có backend thật
     APP_NAME: 'Chatbot Pháp Luật'
 };
